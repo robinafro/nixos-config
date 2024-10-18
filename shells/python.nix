57c4cd20f065a7ba04369ccfec7d4434f78c3ec3
robinafro <oble.ro.2023@skola.ssps.cz>
2024-10-18 15:28:12 +0200

    Remove automatic .venv creation

--- shells/python.nix
+++ shells/python.nix
@@ -27,17 +27,7 @@ in with pkgs; mkShell {
   shellHook = ''
     SOURCE_DATE_EPOCH=$(date +%s)
     export "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${lib-path}"
-    VENV=.venv
-
-    if test ! -d $VENV; then
-      python3.12 -m venv $VENV
-    fi
-    source ./$VENV/bin/activate
     export PYTHONPATH=`pwd`/$VENV/${python.sitePackages}/:$PYTHONPATH
     pip install -r requirements.txt
-  '';
-
-  postShellHook = ''
-    ln -sf ${python.sitePackages}/* ./.venv/lib/python3.12/site-packages
   '';
 }
